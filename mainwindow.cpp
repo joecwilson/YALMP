5a85a7a6fd313e24cd42295bf9964adcdae75344
Joseph Wilson <josephcwilson47@gmail.com>
2024-05-08 14:04:11 -0500

    Initial Commit

---
+++ mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QMediaPlayer>
+#include <QAudioOutput>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QMediaPlayer *player = new QMediaPlayer;
+    QAudioOutput *audioOutput = new QAudioOutput;
+    player->setAudioOutput(audioOutput);
+    // ...
+    player->setSource(QUrl::fromLocalFile("/home/joseph/Music/Anno 1404/1-04-Pirates.mp3"));
+    audioOutput->setVolume(50);
+    player->play();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
