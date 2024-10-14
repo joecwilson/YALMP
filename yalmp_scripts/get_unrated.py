f808abb0fa3211861cd64b9806ba30b1e9e7b0ae
Joseph Wilson <josephcwilson47@gmail.com>
2024-10-14 11:23:57 -0500

    Added get unrated

---
+++ yalmp_scripts/get_unrated.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+from yalmp_scripts.constants import MUSIC_FOLDER
+from yalmp_scripts.utils import get_all_tracks
+
+
+def main():
+    tracks = get_all_tracks()
+    unrated_tracks = []
+    for track in tracks:
+        with open(track) as f:
+            track_dict = json.load(f)
+            if "rating" not in track_dict:
+                unrated_tracks.append(track_dict["path"] + "\n")
+    print(unrated_tracks)
+    with open(os.path.join(MUSIC_FOLDER, "unratedsongs.m3u8"), "w") as f:
+        for track in unrated_tracks:
+            f.write(track)
+
+
+if __name__ == "__main__":
+    main()
--- yalmp_scripts/utils.py
+++ yalmp_scripts/utils.py
@@ -7,3 +7,26 @@ from yalmp_scripts.constants import YALMP_FOLDER
 def get_albums():
     with open(os.path.join(YALMP_FOLDER, "albums.json")) as f:
         return json.load(f)
+
+
+def get_all_tracks():
+    albums = get_albums()
+    discs = []
+    for album in albums:
+        discs.extend(get_discs_from_album(album))
+    tracks = []
+    for disc in discs:
+        tracks.extend(get_tracks_from_disc(disc))
+    return tracks
+
+
+def get_discs_from_album(album):
+    with open(album) as f:
+        album_json = json.load(f)
+        return album_json["discs"]
+
+
+def get_tracks_from_disc(disc):
+    with open(disc) as f:
+        disc_json = json.load(f)
+        return disc_json["tracks"]
